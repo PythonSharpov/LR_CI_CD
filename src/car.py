f4ccbc1700b899e908395421c870872df1934974
Semyon <semengamayunov@mail.ru>
2026-03-17 16:05:43 +0300

    Isprav

--- src/car.py
+++ src/car.py
@@ -1,4 +1,3 @@
-#Comment
 class Car:
     def __init__(self, model: str, fuel_capacity: float) -> None:
         self._model = model
@@ -8,16 +7,19 @@ class Car:
     def get_current_fuel_level(self) -> float:
         return self._fuel_in_tank
 
-    def refuel_car(self, fuel_quantity: float):
+    def refuel_car(self, fuel_quantity: float) -> None:
+        if fuel_quantity <= 0:
+            raise ValueError("Количество топлива должно быть положительным")
         if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
-            raise Exception("Вы пытаетесь залить слишком много бензина!")
+            raise ValueError("Вы пытаетесь залить слишком много бензина!")
         self._fuel_in_tank += fuel_quantity
 
-    def drive(self, distance_km: float):
-        # Считаем, что расход 8 литров на 100 км
-        fuel_burned: int = 8 * (distance_km / 100)
-        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+    def drive(self, distance_km: float) -> float:
+        if distance_km <= 0:
+            raise ValueError("Дистанция должна быть положительной")
+        # Расход 8 литров на 100 км
+        fuel_burned = 8 * (distance_km / 100)
         if self._fuel_in_tank < fuel_burned:
-            raise Exception("Не доедем жеж...")
+            raise ValueError("Не доедем жеж...")
         self._fuel_in_tank -= fuel_burned
         return self.get_current_fuel_level()
